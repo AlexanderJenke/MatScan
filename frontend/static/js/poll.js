c07905258c847f6789c8528d7c09b2597bacef08
Alexander Jenke <alexander.jenke@tu-dresden.de>
2020-03-24 10:36:32 +0100

    add js

---
+++ frontend/static/js/poll.js
@@ -0,0 +1,22 @@
+(function poll() {
+    var poll_interval = 0;
+
+    $.ajax({
+        url: "poll",
+        type: 'GET',
+        dataType: 'json',
+        timeout: 31000,
+        success: function (data, _, jqXHR) {
+            if (jqXHR.status == 200) {
+                update(data)
+            }
+            poll_interval = 0;
+        },
+        error: function () {
+            poll_interval = 10000;
+        },
+        complete: function () {
+            setTimeout(poll, poll_interval);
+        },
+    });
+})();
