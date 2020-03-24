244a29389478f9c72eaa4b645a5be8ebcda629c4
Alexander Jenke <alexander.jenke@tu-dresden.de>
2020-03-24 18:56:47 +0100

    add dummy storage

---
+++ backend/Storage.py
@@ -0,0 +1,7 @@
+class Storage:
+    def __init__(self):
+        self.items = {}
+        pass
+
+    def __getitem__(self, pzn):
+        return "Vividrin 20mg Augentropfen", {202011: 5, 202012: 5, 202201: 20, 202010: 10}
